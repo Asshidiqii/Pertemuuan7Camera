dd46a84738f945443a14e8045b488fb4b4726091
Asshidiqii <mhmmdasshidiqii@gmail.com>
2022-04-11 22:54:21 +0700

    Pertemuan7Camera

---
+++ app/src/main/java/com/ikhwankoto/cameraandcamerax/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.ikhwankoto.cameraandcamerax
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ikhwankoto.cameraandcamerax.useCase.cameraIntent.SimpleCameraActivity
+import com.ikhwankoto.cameraandcamerax.useCase.cameraX.CameraXActivity
+import com.ikhwankoto.cameraandcamerax.useCase.detectFromPicture.DetectFromPictureActivity
+import com.ikhwankoto.cameraandcamerax.useCase.detectText.DetectTextActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn_camera_intent.setOnClickListener {
+            startActivity(Intent(this, SimpleCameraActivity::class.java))
+        }
+
+        btn_camera_x.setOnClickListener {
+            startActivity(Intent(this, CameraXActivity::class.java))
+        }
+
+    }
+
+}
